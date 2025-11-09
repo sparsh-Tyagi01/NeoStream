195bb6b6975e29d035ed42e190cf96e8f168f67a
Sparsh Tyagi <sparshtyagi630@gmail.com>
2025-11-09 20:46:09 +0530

    Update user.js

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -8,7 +8,9 @@ dotenv.config()
 const generateOTP = () => Math.floor(100000 + Math.random() * 900000).toString();
 
 const transporter = nodemailer.createTransport({
-  service: 'gmail',
+  host: 'smtp-relay.brevo.com',
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
