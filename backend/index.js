f6e652019a5a60d4743ab5b5a3db652fb8184c78
Sparsh-tyagi01 <sparshtyagi630@gmail.com>
2026-02-10 23:35:26 +0530

    change port

--- backend/index.js
+++ backend/index.js
@@ -59,7 +59,7 @@ io.on('connection', (socket) => {
   });
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8000;
 server.listen(PORT, () => {
   console.log(`Server started at PORT: ${PORT}`);
 });
